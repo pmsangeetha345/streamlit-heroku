ccff09c9d3760d863e322ed76f66f220243f50e7
pmsangeetha345 <102162344+pmsangeetha345@users.noreply.github.com>
2022-11-28 00:34:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 # Check if the given number is Odd or Even
